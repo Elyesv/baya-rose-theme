41e4bb53ae7d9e0ae0ff1f15cd337f8016110fc2
Elyes Voisin <elyes.voisin@edu.devinci.fr>
2022-01-04 10:26:33 +0100

    INIT file

---
+++ footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package baya-rose
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'baya-rose' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'baya-rose' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'baya-rose' ), 'baya-rose', '<a href="http://elyesvoisin.fr">BAP45</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ front-page.php
@@ -0,0 +1,9 @@
+<?php
+
+get_header();
+
+?>
+
+<div class="">
+    <p>test </p>
+</div>
