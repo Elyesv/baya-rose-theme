2ee7df857e01a869beb479ea1763069f91a4a85e
Enas <enas.elmershati@gmail.com>
2022-01-04 23:17:31 +0100

    Progress HTML and CSS

---
+++ page-nous-connaitre.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package baya-rose
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			?>
+
+
+			<section  class="top">
+				<img src="http://localhost/baya-rose/wp-content/uploads/2022/01/pexels-photo-9512038.png" alt="Baya-Rose">
+				<h1><?php echo the_title( '<h1 class="entry-title"></h1>' );?> </h1>
+				<img class="illustration-ligne" src="http://localhost/baya-rose/wp-content/uploads/2022/01/Illustration-8.png" alt="Baya-Rose">
+			</section>
+
+
+			<section class="connaitre-un">
+				<div class="div-flex width-container">
+				<img src="http://localhost/baya-rose/wp-content/uploads/2022/01/pexels-photo-847483.png" alt="Environnement Liberté">
+					<div>
+						<h2>RESPECT DE L’ENVIRONNEMENT</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+						ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+					</div>
+				</div>
+			</section>
+
+			<section class="connaitre-deux">
+				<div class="div-flex width-container">
+					<div>
+						<h2>RESPECT DE L’ENVIRONNEMENT</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+						ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+					</div>
+				
+						<img src="http://localhost/baya-rose/wp-content/uploads/2022/01/pexels-photo-6412282.png" alt="Environnement Liberté">
+				
+				</div>
+			</section>
+
+
+
+
+
+<?php
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+<?php
+
+get_footer();
